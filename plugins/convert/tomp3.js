596ce708a37e8af1ea100838ad5fbab64c00aa20
neoxr <neoxr@null.net>
2025-10-01 11:04:07 +0000

    chore(deps): bump @neoxr/wb from 6.0.0-rc.8 to 6.0.0-rc.9
    fix: voicenote (audio is not available)
    
    chore: added media

--- plugins/convert/tomp3.js
+++ plugins/convert/tomp3.js
@@ -1,8 +1,6 @@
 import { Converter } from '@neoxr/wb'
 import { readFileSync as read, unlinkSync as remove, writeFileSync as create } from 'fs'
-import path from 'path'
 import { exec } from 'child_process'
-import { tmpdir } from 'node:os'
 
 export const run = {
    usage: ['tomp3', 'tovn'],
@@ -33,34 +31,19 @@ export const run = {
                })
             })
          } else {
-            let q = m.quoted ? m.quoted : m
-            let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
-            if (/ogg/.test(mime)) {
+            const q = m.quoted ? m.quoted : m
+            const mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+            if (/audio|video/.test(mime)) {
                client.sendReact(m.chat, '🕒', m.key)
-               let buffer = await q.download()
-               const media = path.join(tmpdir(), Utils.filename('mp3'))
-               let save = create(media, buffer)
-               const result = Utils.filename('mp3')
-               exec(`ffmpeg -i ${media} ${result}`, async (err, stderr, stdout) => {
-                  remove(media)
-                  if (err) return client.reply(m.chat, Utils.texted('bold', `🚩 Conversion failed.`), m)
-                  let buff = read(result)
-                  if (/tomp3|toaudio/.test(command)) return client.sendFile(m.chat, buff, 'audio.mp3', '', m).then(() => {
-                     remove(result)
-                  })
-                  if (/tovn/.test(command)) return client.sendFile(m.chat, buff, 'audio.mp3', '', m, {
+               if (/tomp3|toaudio/.test(command)) {
+                  const buff = await Converter.toAudio(await q.download())
+                  return client.sendFile(m.chat, buff, 'audio.mp3', '', m)
+               } else if (/tovn/.test(command)) {
+                  const buff = await Converter.toPTT(await q.download())
+                  return client.sendFile(m.chat, buff, '', '', m, {
                      ptt: true
-                  }).then(() => {
-                     remove(result)
                   })
-               })
-            } else if (/audio|video/.test(mime)) {
-               client.sendReact(m.chat, '🕒', m.key)
-               const buff = await Converter.toAudio(await q.download(), 'mp3')
-               if (/tomp3|toaudio/.test(command)) return client.sendFile(m.chat, buff, 'audio.mp3', '', m)
-               if (/tovn/.test(command)) return client.sendFile(m.chat, buff, 'audio.mp3', '', m, {
-                  ptt: true
-               })
+               }
             } else {
                client.reply(m.chat, Utils.texted('bold', `🚩 This feature only for audio / video.`), m)
             }
